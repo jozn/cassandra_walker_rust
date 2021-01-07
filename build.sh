a1dd6b77c6763b49c5bb8845e89a18d76088186e
Hamid <7atashfeshan@gmail.com>
2021-01-07 14:55:43 +0330

    Add debugin output file for better debuging and overview as how this tool works

--- build.sh
+++ build.sh
@@ -11,4 +11,4 @@ echo "======== End of Building Go Binary ========"
 # temp
 #./cassandra_walker  twitter -d "/home/hamid/life/_active/backbone/src/"
 #./cassandra_walker  system -d "/home/hamid/life/_active/backbone/micro/logic/src/"
-./cassandra_walker  twitter -c 37.152.182.28 -d "/home/hamid/life/_active/backbone/micro/logic/src/"
+./cassandra_walker  twitter -c 37.152.182.28 -d "/home/hamid/life/_active/backbone/micro/logic/src/" > debug_gen_sample.txt
