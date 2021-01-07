5ec2cdd68217731f788549fd347e56cf9b24bafd
Hamid <7atashfeshan@gmail.com>
2021-01-07 14:23:58 +0330

    Moved template funcs to same files

--- tpl_func.go
+++ tpl_func.go
@@ -1,5 +1,13 @@
 package main
 
+import "text/template"
+
+func NewTemplateFuncs() template.FuncMap {
+	return template.FuncMap{
+		"ms_to_slice": ms_to_slice,
+	}
+}
+
 func ms_to_slice(typ ...string) []string {
 	return typ
 }
